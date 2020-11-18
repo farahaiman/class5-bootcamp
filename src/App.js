9ace4e6e002241753e38512e304c8fc53a3374bd
farah <farahaiman548@gmail.com>
2020-11-18 08:48:13 +0500

    create file for react context API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>PAKISTAN</h1>
+     <h1>PAKISTAN ZENDABAAD 🏳️ ❤️ </h1>
     </div>
   );
 }
